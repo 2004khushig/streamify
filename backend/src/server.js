2a377490ceb3a2873c6e69f79f16fe5c25749c49
Khushi Gupta <143532781+2004khushig@users.noreply.github.com>
2025-11-01 20:37:53 +0530

    Update CORS origin for production deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const app=express();
 const PORT=process.env.PORT;
 
 app.use(cors({
-    origin:["http://localhost:5173/"],
+    origin:["https://streamify-5-1e6y.onrender.com"],
     credentials:true
 }));
 app.use(express.json());
