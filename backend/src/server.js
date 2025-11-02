21568144406eeade3f345517f6e8bb1989cc4c48
2004khushig <3004khushigupta@gmail.com>
2025-11-02 14:21:21 +0530

    changes done

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import {connectDB} from "./lib/db.js";
+import path from "path";
 dotenv.config();
 import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
@@ -10,9 +11,10 @@ import chatRoutes from "./routes/chat.route.js";
 
 const app=express();
 const PORT=process.env.PORT;
+const __dirname=path.resolve();
 
 app.use(cors({
-    origin:["http://localhost:5173/"],
+    origin:["http://localhost:5173"],
     credentials:true
 }));
 app.use(express.json());
@@ -21,6 +23,12 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/chat",chatRoutes)
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/vite-project/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"../frontend/vite-project/dist/index.html"));
+    });
+}
 
 app.listen(PORT,()=>{
     console.log(`Server is running on port ${PORT}`);
--- frontend/vite-project/src/lib/axios.js
+++ frontend/vite-project/src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from "axios";
+const BASE_URL = import.meta.env.MODE==="development"?"http://localhost:5001/api":"/api";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL + "/api/",
+    baseURL: BASE_URL,
     withCredentials: true,
 });
